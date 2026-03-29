372f1de6b61f9267e3f4ca6892faba9576f98253
micpub <7869939+micpub@users.noreply.github.com>
2026-03-30 01:07:12 +0200

    Update version

--- custom_components/wanas/const.py
+++ custom_components/wanas/const.py
@@ -35,5 +35,5 @@ PLATFORMS = (
 
 URL_BASE = "/wanas"
 WANAS_CARDS = [
-    {"name": "Wanas Cards", "filename": "wanas-vent-card.js", "version": "0.1.3"}
+    {"name": "Wanas Cards", "filename": "wanas-vent-card.js", "version": "0.1.6"}
 ]
